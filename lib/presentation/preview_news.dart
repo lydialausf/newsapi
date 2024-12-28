00cd0d23caa5ca8666832454c60fb938bf6186c9
lydialausf <stam_pin@hotmail.com>
2024-12-28 21:05:38 +0800

    update preview page

--- lib/presentation/preview_news.dart
+++ lib/presentation/preview_news.dart
@@ -14,36 +14,58 @@ class _PreviewNewsState extends State<PreviewNews> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text(widget.news.author.isNotEmpty
+              ? "Article by ${widget.news.author}"
+              : "Article"),
+        ),
         body: SingleChildScrollView(
             child: Column(
           children: [
             widget.news.urlToImage.isNotEmpty
-                ? Image.network(
-                    widget.news.urlToImage,
-                  )
-                : Text('No Image'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                widget.news.title,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                widget.news.description,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                widget.news.content,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
+                ? SizedBox(
+                    width: double.infinity,
+                    height: 400,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(widget.news.urlToImage),
+                            fit: BoxFit.cover),
+                      ),
+                    ))
+                : SizedBox(
+                    width: double.infinity,
+                    height: 400,
+                    child: Text(
+                      'No Image',
+                      textAlign: TextAlign.center,
+                    )),
+            Wrap(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.news.title,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.news.description,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.news.content,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            )
           ],
         )));
   }
